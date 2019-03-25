5d5dc8cf91c06db23eae02a30950609215e0a2d6
fad333 <47511104+fad333@users.noreply.github.com>
2019-03-25 14:55:47 +0800

    Update panel.py

--- panel.py
+++ panel.py
@@ -22,7 +22,7 @@ print ('''\033[1;93m
  \033[91m<3\033[37mBy f4dl
  \033[91m<3\033[37mEnjoy Your Life \033[1;93m''')
  
- print ('\033[1;31mJika ada Kesalahan Resiko Ditanggung sendiri ya kawan :)\033[1;m\n')
+ print ('\033[1;31mResiko Ditanggung sendiri ya kawan :)\033[1;m\n')
  print ('\033[1;31m--------------------------------------------------------------------------\033[1;m\n')
  try:
 	target = target.replace('https://', '') #Removes https://
@@ -31,32 +31,32 @@ except:
 berhenti()
 
 	
-target = target.replace('http://', '') #and http:// from the url
-target = target.replace('/', '') #removes / from url so we can have example.com and not example.com/
-target = 'http://' + target #adds http:// before url so we have a perfect URL now
+target = target.replace('http://', '') 
+target = target.replace('/', '') 
+target = 'http://' + target 
 if args.prefix != None:
 	target = target + args.prefix
 try:
-	r = requests.get(target + '/robots.txt') #Requests to example.com/robots.txt
-	if '<html>' in r.text: #if there's an html error page then its not robots.txt
+	r = requests.get(target + '/robots.txt') 
+	if '<html>' in r.text:
 		print ('  \033[1;31m[-]\033[1;m Robots.txt not found\n')
-	else: #else we got robots.txt
+	else: 
 		print ('  \033[1;32m[+]\033[0m Robots.txt found. Check for any interesting entry\n')
 		print (r.text)
-except: #if this request fails, we are getting robots.txt
+except: 
 	print ('  \033[1;31m[-]\033[1;m Robots.txt not found\n')
 print ('\033[1;31m--------------------------------------------------------------------------\033[1;m\n')
 
 def scan(links):
-	for link in links: #fetches one link from the links list
+	for link in links: 
 		link = target + link # Does this--> example.com/admin/
-		r = requests.get(link) #Requests to the combined url
-		http = r.status_code #Fetches the http response code
-		if http == 200: #if its 200 the url points to valid resource i.e. admin panel
+		r = requests.get(link) 
+		http = r.status_code 
+		if http == 200: 
 			print ('  \033[1;32m[+]\033[0m Admin panel Ditemukan: %s'% link)
-		elif http == 404: #404 means not found
+		elif http == 404: 
 			print ('  \033[1;31m[-]\033[1;m %s'% link)
-		elif http == 302: #302 means redirection
+		elif http == 302: 
 			print ('  \033[1;32m[+]\033[0mPotensi kerentanan EAR ditemukan: ' + link)
 		else:
 			print ('  \033[1;31m[-]\033[1;m %s'% link)
@@ -88,24 +88,24 @@ def get_paths(type):
         print ('\033[1;31m[-]\033[1;m Wordlist Tidak Ditemukan!')
         quit()
 
-if args.fast == True: #if the user has supplied --fast argument
-	type = args.type #gets the input from --type argument
-	get_paths(type) #tells the link grabber to grab links according to user input like php, html, asp
-	paths1 = paths[:len(paths)/2] #The path/links list gets
-	paths2 = paths[len(paths)/2:] #divided into two lists
+if args.fast == True: 
+	type = args.type 
+	get_paths(type) 
+	paths1 = paths[:len(paths)/2]
+	paths2 = paths[len(paths)/2:] 
 	def part1():
-		links = paths1 #it is the first part of the list
-		scan(links) #calls the scanner
+		links = paths1 
+		scan(links)
 	def part2():
-		links = paths2 #it is the second part of the list
-		scan(links) #calls the scanner
+		links = paths2 
+		scan(links) 
 	t1 = threading.Thread(target=part1) 
 	t2 = threading.Thread(target=part2) 
-	t1.start() #starts thread 1
-	t2.start() #starts thread 2
-	t1.join() #Joins both
-	t2.join() #of the threads
-else: #if --fast isn't supplied we go without threads
+	t1.start() 
+	t2.start() 
+	t1.join()
+	t2.join() 
+else: 
 	type = args.type
 	get_paths(type)
 	links = paths
